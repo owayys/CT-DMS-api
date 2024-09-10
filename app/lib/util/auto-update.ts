56f401d180b04b5094ffe9dcf91695f65025b563
owayys <owais.ahsan@hotmail.com>
2024-09-10 20:07:28 +0500

    Decorator to automatically update the updatedAt timestamp for entities on change

---
+++ app/lib/util/auto-update.ts
@@ -0,0 +1,34 @@
+export function AutoUpdate() {
+    return function (
+        target: any,
+        propertKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        const method = descriptor.value;
+
+        descriptor.value = function (...args: any[]) {
+            const result = method.apply(this, args);
+            if (typeof this.onUpdate === "function") {
+                this.onUpdate();
+            }
+            return result;
+        };
+        return descriptor;
+    };
+}
+
+// export function AutoUpdate(
+//     target: Function,
+//     propertKey: string,
+//     // descriptor: PropertyDescriptor
+//     value: any
+// ) {
+//     return (...args: any[]) => {
+//         const result = target.apply(value, args);
+//         if (typeof value.onUpdate === "function") {
+//             console.log("UPDATE");
+//             value.onUpdate();
+//         }
+//         return result;
+//     };
+// }
