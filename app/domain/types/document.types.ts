48d9ce83167ee92655d4ca01aac2c07770002d51
owayys <owais.ahsan@hotmail.com>
2024-09-10 19:54:09 +0500

    User Defined Document Metadata Type

---
+++ app/domain/types/document.types.ts
@@ -0,0 +1,36 @@
+import { TagEntity } from "../entities/tag.entity";
+import { UUID } from "../value-objects/uuid.value-object";
+
+export type DocumentProps<T> = {
+    userId: UUID;
+    fileName: string;
+    fileExtension: string;
+    content: string;
+    contentType: string;
+    tags: TagEntity[];
+    meta: { [Property in keyof T]: T[Property] } | null;
+};
+
+export type CreateDocumentProps<T> = {
+    userId: UUID;
+    fileName: string;
+    fileExtension: string;
+    content: string;
+    contentType: string;
+    tags: {
+        key: string;
+        name: string;
+    }[];
+    meta: { [Property in keyof T]: T[Property] } | null;
+};
+
+type Primitive = string | boolean | number;
+
+export type UserDefinedMetadata = {
+    [key: string]: Primitive | Array<Primitive> | UserDefinedMetadata;
+};
+
+export type UpdateDocumentProps = Omit<
+    DocumentProps<UserDefinedMetadata>,
+    "userId"
+>;
