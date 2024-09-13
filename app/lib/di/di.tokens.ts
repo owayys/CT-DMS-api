166a69950cb91257ec64227eda43645fd6c403da
owayys <owais.ahsan@hotmail.com>
2024-09-13 13:12:58 +0500

    Registered Domain services with DI Container

--- app/lib/di/di.tokens.ts
+++ app/lib/di/di.tokens.ts
@@ -7,11 +7,20 @@ export const USER_SERVICE = Symbol("USER_SERVICE");
 export const USER_CONTROLLER = Symbol("USER_CONTROLLER");
 export const USER_MAPPER = Symbol("USER_MAPPER");
 
+export const REGISTER_USER_SERVICE = Symbol("REGISTER_USER_SERVICE");
+export const LOGIN_USER_SERVICE = Symbol("LOGIN_USER_SERVICE");
+
 export const DOCUMENT_REPOSITORY = Symbol("DOCUMENT_REPOSITORY");
 export const DOCUMENT_SERVICE = Symbol("DOCUMENT_SERVICE");
 export const DOCUMENT_CONTROLLER = Symbol("DOCUMENT_CONTROLLER");
 export const DOCUMENT_MAPPER = Symbol("DOCUMENT_MAPPER");
 export const TAG_MAPPER = Symbol("TAG_MAPPER");
+
+export const AUTHORIZE_DOCUMENT_ACCESS_SERVICE = Symbol(
+    "AUTHORIZE_DOCUMENT_ACCESS_SERVICE"
+);
+
+export const FILE_HANDLER = Symbol("FILE_HANDLER");
 
 export const JWT_SERVICE = Symbol("JWT_SERVICE");
 export const JWT_CONTROLLER = Symbol("JWT_CONTROLLER");
--- app/lib/di/index.ts
+++ app/lib/di/index.ts
@@ -1,33 +1,43 @@
 import { Container } from "./Container";
 // import { DrizzleUserRepository } from "../../repositories/DrizzleUserRepository";
-import { UserService } from "../../services/user.service";
+// import { UserService } from "../../services/user.service";
+import { UserService } from "../../application/services/user.service";
 // import { UserController } from "../../controllers/user.controller";
 // import { DrizzleDocumentRepository } from "../../repositories/DrizzleDocumentRepository";
-import { DocumentService } from "../../services/document.service";
+// import { DocumentService } from "../../services/document.service";
+import { DocumentService } from "../../application/services/document.service";
 // import { DocumentController } from "../../controllers/document.controller";
 import {
+    AUTHORIZE_DOCUMENT_ACCESS_SERVICE,
     DATABASE,
     DOCUMENT_MAPPER,
     // DOCUMENT_CONTROLLER,
     DOCUMENT_REPOSITORY,
     DOCUMENT_SERVICE,
+    FILE_HANDLER,
     // JWT_CONTROLLER,
     JWT_SERVICE,
     LOGGER,
+    LOGIN_USER_SERVICE,
+    REGISTER_USER_SERVICE,
     TAG_MAPPER,
     USER_MAPPER,
     // USER_CONTROLLER,
     USER_REPOSITORY,
     USER_SERVICE,
 } from "./di.tokens";
-import { DrizzleClientWrapper } from "../../database";
-import { JWTService } from "../../services/jwt.service";
+import { DrizzleClientWrapper } from "../../infrastructure/database";
+import { JWTService } from "../../application/services/jwt.service";
 import { BunyanLogger } from "../logging/BunyanLogger";
-import { UserMapper } from "../../mappers/user.mapper";
-import { DocumentMapper } from "../../mappers/document.mapper";
-import { TagMapper } from "../../mappers/tag.mapper";
-import { UserRepository } from "../../database/user.repository";
-import { DocumentRepository } from "../../database/document.repository";
+import { UserMapper } from "../../infrastructure/mappers/user.mapper";
+import { DocumentMapper } from "../../infrastructure/mappers/document.mapper";
+import { TagMapper } from "../../infrastructure/mappers/tag.mapper";
+import { UserRepository } from "../../infrastructure/repositories/user.repository";
+import { DocumentRepository } from "../../infrastructure/repositories/document.repository";
+import { FileHandlerService } from "../../infrastructure/services/file-handler.service";
+import { RegisterUserService } from "../../domain/services/register-user.service";
+import { LoginUserService } from "../../domain/services/login-user.service";
+import { AuthorizeDocumentAccessService } from "../../domain/services/authorize-document-access.service";
 // import { JWTController } from "../../controllers/jwt.controller";
 Container.register(LOGGER, BunyanLogger);
 
@@ -39,12 +49,22 @@ Container.register(USER_SERVICE, UserService);
 // Container.register(USER_CONTROLLER, UserController);
 Container.register(USER_MAPPER, UserMapper);
 
+Container.register(REGISTER_USER_SERVICE, RegisterUserService);
+Container.register(LOGIN_USER_SERVICE, LoginUserService);
+
 // Container.register(DOCUMENT_REPOSITORY, DrizzleDocumentRepository);
 Container.register(DOCUMENT_REPOSITORY, DocumentRepository);
 Container.register(DOCUMENT_SERVICE, DocumentService);
 // Container.register(DOCUMENT_CONTROLLER, DocumentController);
 Container.register(DOCUMENT_MAPPER, DocumentMapper);
 Container.register(TAG_MAPPER, TagMapper);
+
+Container.register(
+    AUTHORIZE_DOCUMENT_ACCESS_SERVICE,
+    AuthorizeDocumentAccessService
+);
+
+Container.register(FILE_HANDLER, FileHandlerService);
 
 Container.register(JWT_SERVICE, JWTService);
 // Container.register(JWT_CONTROLLER, JWTController)
