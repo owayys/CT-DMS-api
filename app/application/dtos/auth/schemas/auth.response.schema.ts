6dd5486486bc69814f412e3c0bd67aa05713a538
owaisatcarbonteq <owais.ahsan@carbonteq.com>
2025-02-17 19:23:53 +0500

    Added schemas

---
+++ app/application/dtos/auth/schemas/auth.response.schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const AuthResponse = z.object({
+    accessToken: z.string(),
+    refreshToken: z.string().optional(),
+});
--- app/lib/validators/common.ts
+++ app/lib/validators/common.ts
@@ -1,7 +1,30 @@
+import { UploadedFile } from "express-fileupload";
 import { z } from "zod";
+
+export const UploadedFileSchema = z.object({
+    name: z.string(),
+    encoding: z.string(),
+    mimetype: z.string(),
+    data: z.instanceof(Buffer),
+    tempFilePath: z.string(),
+    truncated: z.boolean(),
+    size: z.number(),
+    md5: z.string(),
+    mv: z.function().args(z.string()).returns(z.promise(z.void())),
+}) satisfies z.ZodType<UploadedFile>;
+
+export const BaseResponse = z.object({
+    Id: z.string(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+});
 
 export const UpdateResponse = z.object({
     success: z.boolean(),
+});
+
+export const ErrorResponse = z.object({
+    error: z.object({ message: z.string() }),
 });
 
 export const DeleteResponse = UpdateResponse;
