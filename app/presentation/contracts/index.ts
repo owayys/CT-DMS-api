d8fa00b32a4bc8365352becf912f8ceb830dda27
owaisatcarbonteq <owais.ahsan@carbonteq.com>
2025-02-17 19:49:58 +0500

    Exposed auth contract

--- app/presentation/contracts/index.ts
+++ app/presentation/contracts/index.ts
@@ -5,10 +5,12 @@ import {
 } from "@orpc/contract";
 import { userContract } from "./user.contract";
 import { documentContract } from "./document.contract";
+import { authContract } from "./auth.contract";
 
 export const contract = oc.router({
     user: userContract,
     document: documentContract,
+    auth: authContract,
 });
 
 export type Inputs = InferContractRouterInputs<typeof contract>;
